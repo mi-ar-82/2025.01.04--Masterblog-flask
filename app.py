efadf5bf4f78e8fbaf5e34a39563ba7b653068be
m <m@m.com>
2025-01-04 15:11:17 +0100

    Design the Data Structure for Blog Posts

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import json
 
 app = Flask(__name__)
 
 
+# Load blog posts from JSON file
+def load_posts():
+    with open('blog_posts.json', 'r') as file:
+        return json.load(file)
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()  # Fetch blog posts from JSON
+    return render_template('index.html', posts=blog_posts)
 
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
+    
+ 
